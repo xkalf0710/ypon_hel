5f056d014e05fa8a73fa383d90b057696c1e0fa5
xkalf0710 <mnymsuren37@gmail.com>
2022-10-11 17:21:04 +0800

    1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+class App extends Component{
+
+  constructor(){
+    super();
+    this.state = {
+      name: {firstname: 'Nymsuren', lastname: 'Monkherdenekhuyg'},
+      company: 'Vulcan'
+    }
+  }
+
+  render(){
+    return(
+      <div className="App"> 
+      <header className='App-header'> 
+         <img src={logo} className='App-logo' alt='logo' />
+         <p>Hi {this.state.name}, I work at {this.state.company}</p>
+         <button onClick={()=> {
+           this.setState(()=> {
+             return {
+               name: {firstname: "Nymka", lastname: 'Munkh'},
+             };
+           }, ()=> {console.log(this.state);});
+         }}>Change button</button>
       </header>
-    </div>
-  );
+      </div>     
+    );
+  }
 }
 
 export default App;
