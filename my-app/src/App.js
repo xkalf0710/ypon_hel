9db5fe22df3cd1eea84e144a447b2f427bba2fe0
xkalf0710 <mnymsuren37@gmail.com>
2022-10-14 10:14:21 +0800

    component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,33 +1,64 @@
-import './App.css';
 import { Component } from 'react';
-import logo from './logo.svg';
-
-
+import './App.css';
 class App extends Component{
 
   constructor(){
     super();
     this.state = {
-      name: {firstname: 'Nymsuren', lastname: 'Monkherdenekhuyg'},
-      company: 'Vulcan'
-    }
+      monsters: [],
+      searchField: '',
+    };
+    console.log("constructor");
   }
+  // componentDidMount() is invoked immediately after a component is mounted (inserted into the tree). Initialization that requires DOM nodes should go here. If you need to load data from a remote endpoint, this is a good place to instantiate the network request.
+ // componentDidMount() ashiglan database holboj bolnoo
+
+  componentDidMount(){
+    console.log('componentDidMount');
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState(()=> {
+      return {monsters: users};
+    }, 
+    ()=> {
+      console.log(this.state);
+    }));
+  }
+  onSearchChange = (event)=> {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return {searchField};
+    });
+  };
 
   render(){
+    console.log('render');
+    const { monsters, searchField } = this.state;
+
+    const { onSearchChange } = this;
+
+
+    const filteredMonsters = monsters.filter((monsters)=> {
+      return monsters.name.toLocaleLowerCase().includes(searchField);
+     });
+
     return(
-      <div className="App"> 
-      <header className='App-header'> 
-         <img src={logo} className='App-logo' alt='logo' />
-         <p>Hi {this.state.name}, I work at {this.state.company}</p>
-         <button onClick={()=> {
-           this.setState(()=> {
-             return {
-               name: {firstname: "Nymka", lastname: 'Munkh'},
-             };
-           }, ()=> {console.log(this.state);});
-         }}>Change button</button>
-      </header>
-      </div>     
+      <div className='App'> 
+      <input className='search-box' type='search' placeholder='search monsters' 
+      onChange={
+      onSearchChange
+      }/>
+      {/* mapping arrays to elements  */}
+        {filteredMonsters.map((monsters) => {
+           return (
+             <div key={monsters.id}>
+               <h1>{monsters.name}</h1> 
+               </div>
+           );
+        })
+        }
+        
+      </div>
     );
   }
 }
