0c048cc1836ebf4308a02f2a6acc7da19da93119
xkalf0710 <mnymsuren37@gmail.com>
2022-10-31 14:38:45 +0800

    title

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,12 +13,14 @@ import './App.css';
 const App = () => {
   
   const [searchField, setSearchField] = useState('a'); //value, setvalue 
+  const [title, setTitle] = useState('');
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
   
+  console.log('rendered');
 
   useEffect(() => {
-    console.log('effect fired');
+    
   fetch('https://jsonplaceholder.typicode.com/users')
   .then((response) => response.json())
   .then((users) => 
@@ -39,12 +41,19 @@ const App = () => {
     const searchFieldString = event.target.value.toLocaleLowerCase();
     setSearchField(searchFieldString);
   };
+
+  const onTitleChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setTitle(searchFieldString);
+  };
   return (
     <div className='App'> 
-        <h1 className='app-title'>Monsters Rolodex</h1>
+        <h1 className='app-title'>{title}</h1>
         <SearchBox className='monsters-search-box' onChangeHandler={onSearchChange} placeholder='search monsters'>
         </SearchBox>
-       
+        <br></br>
+        <SearchBox className='title-search-box' onChangeHandler={onTitleChange} placeholder='set title'>
+        </SearchBox>
         <CardList monsters={filteredMonsters}/>
     </div>
   );
